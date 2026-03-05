5844ad89ff03cccdee52a38198054aef7d994817
yashpanchal01 <yashpanchal251198@gmail.com>
2026-03-05 20:09:06 +0530

    Started java course, done variables

---
+++ Course/Main.java
@@ -0,0 +1,11 @@
+package Course;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I like pizza!");
+    }
+    
+    
+}
---
+++ rockpaper.java
@@ -0,0 +1,5 @@
+public class rockpaper {
+    public static void main(String[] args) {
+        System.err.println("Hellow world!");
+    }
+}
